af6adc708cf22347f8d3887edf69e9a86cefa093
William Ross <will@spanner.org>
2015-04-29 21:38:49 +0100

    ssl options merged and passed through to rest_client

--- lib/airborne/base.rb
+++ lib/airborne/base.rb
@@ -24,32 +24,32 @@ module Airborne
     RSpec.configuration
   end
 
-  def get(url, headers = nil)
-    set_response(make_request(:get, url, {headers: headers}))
+  def get(url, headers = nil, ssl_options = {})
+    set_response(make_request(:get, url, {headers: headers}.merge(ssl_options)))
   end
 
-  def post(url, post_body = nil, headers = nil)
-    set_response(make_request(:post, url, {body: post_body, headers: headers}))
+  def post(url, post_body = nil, headers = nil, ssl_options = {})
+    set_response(make_request(:post, url, {body: post_body, headers: headers}.merge(ssl_options)))
   end
 
-  def patch(url, patch_body = nil, headers = nil )
-    set_response(make_request(:patch, url, {body: patch_body, headers: headers}))
+  def patch(url, patch_body = nil, headers = nil, ssl_options = {})
+    set_response(make_request(:patch, url, {body: patch_body, headers: headers}.merge(ssl_options)))
   end
 
-  def put(url, put_body = nil, headers = nil )
-    set_response(make_request(:put, url, {body: put_body, headers: headers}))
+  def put(url, put_body = nil, headers = nil, ssl_options = {})
+    set_response(make_request(:put, url, {body: put_body, headers: headers}.merge(ssl_options)))
   end
 
-  def delete(url, headers = nil)
-    set_response(make_request(:delete, url, {headers: headers}))
+  def delete(url, headers = nil, ssl_options = {})
+    set_response(make_request(:delete, url, {headers: headers}.merge(ssl_options)))
   end
 
-  def head(url, headers = nil)
-    set_response(make_request(:head, url, {headers: headers}))
+  def head(url, headers = nil, ssl_options = {})
+    set_response(make_request(:head, url, {headers: headers}.merge(ssl_options)))
   end
 
-  def options(url, headers = nil)
-    set_response(make_request(:options, url, {headers: headers}))
+  def options(url, headers = nil, ssl_options = {})
+    set_response(make_request(:options, url, {headers: headers}.merge(ssl_options)))
   end
 
   def response
--- lib/airborne/rest_client_requester.rb
+++ lib/airborne/rest_client_requester.rb
@@ -3,26 +3,21 @@ require 'rest_client'
 module Airborne
   module RestClientRequester
     def make_request(method, url, options = {})
-      headers = {content_type: :json}.merge(options[:headers] || {})
       base_headers = Airborne.configuration.headers || {}
-      headers = base_headers.merge(headers)
-      res = if method == :post || method == :patch || method == :put
-        begin
-          request_body = options[:body].nil? ? "" : options[:body]
-          request_body = request_body.to_json if options[:body].is_a?(Hash)
-          RestClient.send(method, get_url(url), request_body, headers)
-        rescue RestClient::Exception => e
-          e.response
-        end
-      else
-        begin
-          RestClient.send(method, get_url(url), headers)
-        rescue RestClient::Exception => e
-          e.response
-        end
-
+      options[:headers] ||= {}
+      options[:headers].reverse_merge!(base_headers).merge!({content_type: :json})
+      options[:url]= get_url(url)
+      options[:method] = method
+      if method == :post || method == :patch || method == :put
+        options[:payload] ||= options[:body] || ""
+        options[:payload] = options[:payload].to_json if options[:payload].is_a?(Hash)
       end
-      res
+      begin
+        res = RestClient::Request.new(options).execute()
+        res
+      rescue RestClient::Exception => e
+        e.response
+      end
     end
   end
 end
